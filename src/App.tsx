000ead13bd512c33b088a9323dae5dcd724cde2a
peter <igbafes8@gmail.com>
2026-01-18 21:31:31 +0100

    feat:brand and setting page

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import ProtectedRoute from "./routes/ProtectedRoute";
 import { Navigate } from "react-router-dom";
 
 import LaptopQRScanPage from "./pages/QRScanPage";
+import SettingsPage from "./pages/Settings";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
         <Route element={<ProtectedRoute />}>
           <Route path="/dashboard" element={<LaptopDashboard />} />
           <Route path="/inventory" element={<Inventory />} />
+          <Route path="/settings" element={<SettingsPage />} />
 
           <Route
             path="/laptops/qr/:serialNumber"
